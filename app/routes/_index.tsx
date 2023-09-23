cc401e7f47ce66d43d2ad48375c190cff922fe82
phbord <phbord@gmail.com>
2023-09-23 09:29:02 +0200

    github page added

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,8 +2,8 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Portfolio de Pierre-Henri Bord" },
+    { name: "description", content: "Portofolio de Pierre-Henri Bord, développeur front-end, intégrateur HTML, React, Nextjs, Remix, Vue, Javascript, Typescript" },
   ];
 };
 

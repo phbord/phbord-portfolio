4e70410b557f3692a75a5cfeb654bcd0b58c1e38
phbord <phbord@gmail.com>
2023-10-07 09:45:30 +0200

    index

--- app/components/ui/BackgroundImageHeader.tsx
+++ app/components/ui/BackgroundImageHeader.tsx
@@ -17,34 +17,45 @@ export default function BackgroundImageHeader({imgUrl}) {
           <BackgroundImage containerClassName='bg-img-header'
                             contentClassName='bg-img-header-body'
                             imgUrl={imgUrl}>
-            <figure className='mt-[4rem] flex items-center'>
-              <span className='sr-only'>{t('logoProfileText')}</span>
-              <img src="/images/ph-profile.png" alt={t('logoProfileText')} className='w-[4rem] h-[4rem] mr-3' />
-              <figcaption className='text-base'>
-                <p>
-                  {
-                    keywordsData?.map((item) =>(
-                      <span key={uuidv4()} className='mr-2'>
-                        {item}
-                      </span>
-                    ))
-                  }
-                </p>
-                <ul className="mt-1 flex">
-                  {
-                    socialNetworkData?.map((item) =>(
-                      <ItemListLayout key={uuidv4()}
-                                data={item} 
-                                itemClass={''} 
-                                linkClass={''} 
-                                imgClass={'w-5 mr-2'} 
-                                textClass={'sr-only'} 
-                                imgSrc={`/images/svg/${item.picto}`} />
-                    ))
-                  }
-                </ul>
-              </figcaption>
-            </figure>
+            <div className="h-full mt-[4rem] flex flex-col justify-between">
+              {/* HAUT */}
+              <figure className='flex items-center'>
+                {/* COLONNE GAUCHE */}
+                <span className='sr-only'>{t('logoProfileText')}</span>
+                <img src="/images/ph-profile.png" alt={t('logoProfileText')} className='w-[4rem] h-[4rem] mr-3' />
+
+                {/* COLONNE DROITE */}
+                <figcaption className='bg-img-header-figcaption'>
+                  <h2 className="mb-1 text-[1.35rem] text-yellow-200">
+                    {t('position')}
+                  </h2>
+                  <p>
+                    {
+                      keywordsData?.map((item) =>(
+                        <span key={uuidv4()} className='mr-2'>
+                          {item}
+                        </span>
+                      ))
+                    }
+                  </p>
+                </figcaption>
+              </figure>
+
+              {/* BAS */}
+              <ul className="mt-1 flex">
+                {
+                  socialNetworkData?.map((item) =>(
+                    <ItemListLayout key={uuidv4()}
+                              data={item} 
+                              itemClass={''} 
+                              linkClass={''} 
+                              imgClass={'w-5 mr-2'} 
+                              textClass={'sr-only'} 
+                              imgSrc={`/images/svg/${item.picto}`} />
+                  ))
+                }
+              </ul>
+            </div>
           </BackgroundImage>
         )
       }
--- app/components/ui/ItemListKnowledges.tsx
+++ app/components/ui/ItemListKnowledges.tsx
@@ -24,33 +24,48 @@ export default function ItemListKnowledges({data, noData, lang}) {
       {
         data
           ? (
-            <li className='mb-4 px-4 py-2 flex justify-between flex-col sm:flex-row border border-dotted bg-neutral-100'>
-              <div className="text-neutral-600">
+            <li className='mb-6'>
+              <h3 className='h3'>
+                {lang === 'fr' ? data.title_fr : data.title_en}
+              </h3>
+              <ul className='flex flex-wrap border-t border-dotted border-orange-500'>
+                {
+                  data?.list?.map((item) => (
+                    <li key={uuidv4()} 
+                        className='mx-3 my-[.375rem] flex items-stretch'>
+                      <figure className='flex flex-col flex-nowrap items-center justify-end'>
+                        <img src={`/images/svg/${item.picto}`} 
+                                alt={item.name} 
+                                className='max-w-[1.5rem] mb-1' />
+                        <figcaption className='text-cyan-900'>{item.name}</figcaption>
+                      </figure>
+                    </li>
+                  ))
+                }
+              </ul>
+              
+              {/* <div className="text-neutral-600">
                 {lang === 'fr' ? data.title_fr : data.title_en}
               </div>
               <ul className='flex ml-2'>
                 {
                   data?.list?.map((item, index) => (
                     <li key={uuidv4()} className=''>
-                      {/* BUTTON */}
                       <Button className='group relative overflow-visible transition-all' 
                               onMouseOver={() => onMouseOver(index, item.name)} 
                               onMouseOut={() => onMouseOut(index)}>
-                        {/* IMAGE */}
                         <img src={`/images/svg/${item.picto}`} 
                               alt={item.name} 
                               className='h-5 ml-2 my[.5rem]' />
-                        {/* TOOLTIP */}
                         {
                           newIndex === index 
                             && <Tooltip name={newName} className='tooltips-knowledges' />
                         }
                       </Button>
-                      {/* BUTTON : end */}
                     </li>
                   ))
                 }
-              </ul>
+              </ul> */}
             </li>
           )
           : (

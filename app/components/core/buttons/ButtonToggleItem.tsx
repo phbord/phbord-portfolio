09661ed1eddc36c37396ce01e868fb992b081942
phbord <phbord@gmail.com>
2023-11-12 18:44:57 +0100

    experiences

--- app/components/core/buttons/ButtonToggleItem.tsx
+++ app/components/core/buttons/ButtonToggleItem.tsx
@@ -1,7 +1,9 @@
 import { useTranslation } from 'react-i18next';
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import { useState } from 'react';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
 
-import Button from '~/components/core/buttons/Button'
+import Button from '~/components/core/buttons/Button';
+import Tooltip from '~/components/core/Tooltip';
 
 
 interface ButtonToggleItemInterface {
@@ -12,12 +14,26 @@ interface ButtonToggleItemInterface {
 
 export default function ButtonToggleItem({isItemOpened, onClick}: ButtonToggleItemInterface) {
   const { t } = useTranslation();
+  const [isNewTooltipOpened, setIsNewTooltipOpened] = useState(false);
+
+  const handleNewMouseOver = () => setIsNewTooltipOpened(true);
+  const handleNewMouseOut = () => setIsNewTooltipOpened(false);
+
 
   return (
     <Button onClick={onClick} 
             srOnlyText={t('toggleItemButtonText')}
-            className='btn-admin-form btn-admin-form--new ml-1'>
+            className='relative btn-admin-form btn-admin-form--new ml-1'
+            onMouseOver={handleNewMouseOver}
+            onMouseOut={handleNewMouseOut}>
       <ChevronDownIcon className={isItemOpened ? 'icon-chevron icon-chevron--opened' : 'icon-chevron'} />
+      {/* TOOLTIPS */}
+      {
+        isNewTooltipOpened && (
+          <Tooltip name={t('buttonToggleText')} 
+                    className='tooltips-footer' />
+        )
+      }
     </Button>
   )
 }
